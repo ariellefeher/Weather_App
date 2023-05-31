70c120e85b220ee4b0f5299f70c7e058e2835af7
ariellefeher <117110135+ariellefeher@users.noreply.github.com>
2023-05-31 18:00:52 +0300

    browser app works, compiling isnt working

--- myWeatherApp/capacitor.config.ts
+++ myWeatherApp/capacitor.config.ts
@@ -1,9 +1,9 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.weatherApp',
-  appName: 'weatherApp',
-  webDir: 'build',
+  appId: 'ionic.io.starter',
+  appName: 'myWeatherApp',
+  webDir: 'dist',
   server: {
     androidScheme: 'https'
   }
